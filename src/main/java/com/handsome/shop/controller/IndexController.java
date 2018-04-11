cfda54d07def63f0924f5d4ad84f77ebf955fd82
wangrongjun <15521302230@163.com>
2018-04-12 01:18:17 +0800

    初步使用SpringMVC，可以访问首页

--- src/main/java/com/handsome/shop/controller/IndexController.java
+++ src/main/java/com/handsome/shop/controller/IndexController.java
@@ -1,8 +1,21 @@
 package com.handsome.shop.controller;
 
+import com.handsome.shop.bean.Goods;
+import com.handsome.shop.bean.GoodsType;
+import com.handsome.shop.dao.GoodsDao;
+import com.handsome.shop.dao.GoodsTypeDao;
+import com.handsome.shop.framework.ActionSupport;
+import com.handsome.shop.framework.DaoFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * by wangrongjun on 2017/11/2.
@@ -10,9 +23,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/a.test", method = RequestMethod.GET)
-    public String testAAA() {
-        return "test";
-    }
+    @GetMapping("/showIndex.do")
+    public String showIndex(
+            HttpServletRequest request,
+            @RequestParam(defaultValue = "0") int pageIndex,
+            @RequestParam(defaultValue = "0") int sortType) {
+        GoodsTypeDao goodsTypeDao = DaoFactory.getGoodsTypeDao();
+        List<GoodsType> goodsTypeList = goodsTypeDao.queryAll();
+        GoodsDao goodsDao = DaoFactory.getGoodsDao();
+        List<Goods> goodsList = goodsDao.begin(12 * pageIndex).count(12).sortType(sortType).queryAll();
+        long totalCount = goodsDao.queryCount(null);
 
+        request.getSession().setAttribute("goodsTypeList", goodsTypeList);
+        request.setAttribute("pageIndex", pageIndex);
+        request.setAttribute("totalCount", totalCount);
+        request.setAttribute("goodsList", goodsList);
+        request.setAttribute("sortType", sortType);
+        return "index";
+    }
 }
